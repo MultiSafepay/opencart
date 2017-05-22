eafef2992aa827e85c7057bd6fa0457a6275d406
Ruud Jonk <ruud@multisafepay.com>
2017-05-22 13:48:39 +0200

    Added disclaimer to source

--- admin/controller/extension/payment/multisafepay.php
+++ admin/controller/extension/payment/multisafepay.php
@@ -1,11 +1,24 @@
 <?php
 
 /**
- * Multisafepay  Payment Integration
- * Date 31-11-2014
- * @version 2.0
- * @package 2.0
- * @copyright 2014 MultiSafePay.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the MultiSafepay plugin
+ * to newer versions in the future. If you wish to customize the plugin for your
+ * needs please document your changes and make backups before you update.
+ *
+ * @category    MultiSafepay
+ * @package     Connect
+ * @author      TechSupport <techsupport@multisafepay.com>
+ * @copyright   Copyright (c) 2017 MultiSafepay, Inc. (http://www.multisafepay.com)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 class ControllerExtensionPaymentMultiSafePay extends Controller
 {
