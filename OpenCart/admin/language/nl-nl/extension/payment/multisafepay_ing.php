47ac4b302a2441974b960974bb638a361f70d6de
Ronald Schonewille <RSchonewille@office.multisafepay.com>
2018-09-19 12:46:55 +0200

    PLGOPNS-91: Rename ING Homepay to ING Home'Pay

--- OpenCart/admin/language/nl-nl/extension/payment/multisafepay_ing.php
+++ OpenCart/admin/language/nl-nl/extension/payment/multisafepay_ing.php
@@ -1,6 +1,6 @@
 <?php
 
-$_['heading_title'] = 'MultiSafepay ING Homepay';
+$_['heading_title'] = 'MultiSafepay ING Home\'Pay';
 
 // Text
 $_['text_multisafepay_ing'] = '<a onclick="window.open(\'http://www.multisafepay.com\');"><img src="view/image/payment/multisafepay.png" alt="MultiSafepay" title="MultiSafepay" style="border: 1px solid #EEEEEE;" /></a>';
