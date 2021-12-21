b048cd8384227a9d2e1eacf044b997353cb44e03
Daniel Civit <daniel.civit@multisafepay.com>
2021-12-21 11:00:31 +0100

    PLGOPNS-443: Improvement in dutch translations (#209)

--- src/catalog/language/dutch/payment/multisafepay.php
+++ src/catalog/language/dutch/payment/multisafepay.php
@@ -2,7 +2,7 @@
 
 $_['text_title']                    	    = 'MultiSafepay';
 $_['text_testmode']						    = 'Waarschuwing: De betalingsgateway staat in \'Testmodus\'.';
-$_['text_order_description']			    = 'Bestel ID# %s van %s geplaatst op %s';
+$_['text_order_description']			    = 'Order ID# %s van %s geplaatst op %s';
 $_['text_customer_balance']				    = 'Klantensaldo - Krediet';
 $_['text_fixed_product_name']			    = 'Vaste belasting: %s';
 $_['text_coupons_name']					    = 'Kortingscoupon: %s';
@@ -17,8 +17,8 @@ $_['text_instructions']                     = 'Betalingsinstructies';
 $_['text_payment_link']                     = 'Klik op de volgende link en voltooi de betaling: <a href="%s">%s</a>';
 $_['text_payment_link_admin_order_history'] = 'De volgende betalingslink is gegenereerd: <a target="_blank" href="%s">%s</a>';
 $_['button_confirm_with_issuer']            = 'Bevestig de bestelling en betaal met iDEAL via ';
-$_['entry_issuer']                          = 'Uitgever';
-$_['entry_gender']                          = 'Behandeling';
+$_['entry_issuer']                          = 'Issuer';
+$_['entry_gender']                          = 'Geslacht';
 $_['entry_date_of_birth']                   = 'Geboortedatum';
 $_['entry_bank_account']                    = 'Bankrekening IBAN';
 $_['entry_account_holder_name']             = 'Rekeninghouder';
@@ -79,7 +79,7 @@ $_['text_title_yourgift']                   = 'YourGift';
 $_['text_comment_callback']                 = 'Notificatie van MultiSafepay voor Order ID %s op %s met status: %s (PSP ID: %s).';
 $_['text_error']                            = 'Als de betaling niet kan worden verwerkt, is er een fout opgetreden. Probeer het opnieuw, als de fout blijft bestaan, neem dan contact op met de webshop.';
 $_['text_error_on_submit']                  = 'Controleer het betalingsformulier zorgvuldig op fouten.';
-$_['text_error_issuer_not_selected']        = 'Uitgever is een verplicht veld';
+$_['text_error_issuer_not_selected']        = 'Issuer is een verplicht veld';
 $_['text_error_apple_pay']                  = 'Apple Pay is niet beschikbaar voor dit apparaat. Ga terug en selecteer een andere betaalmethode';
 $_['text_error_empty_gender']               = 'Geslacht is een verplicht veld';
 $_['text_error_empty_date_of_birth']        = 'Geboortedatum is een verplicht veld';
